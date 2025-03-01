4398112e425299aaa8bc54f2bf091719480143ea
Hariharan <146200143+Hariharan4044@users.noreply.github.com>
2025-03-01 07:34:49 +0530

    Update health.py

--- health.py
+++ health.py
@@ -46,7 +46,7 @@ if uploaded_file is not None:
     st.image(image, caption="Uploaded Image.", use_column_width=True)
 
 
-submit=st.button("Tell me the total calories")
+submit=st.button("Tell me total calories")
 
 input_prompt="""
 You are an expert in nutritionist where you need to see the food items from the image
