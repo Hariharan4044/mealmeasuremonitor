2aeb629695b88aecfa243e216374e0f94c13d71b
Hariharan <146200143+Hariharan4044@users.noreply.github.com>
2025-06-26 21:50:17 +0530

    Update health.py

--- health.py
+++ health.py
@@ -31,7 +31,7 @@ def input_image_setup(uploaded_file):
         ]
         return image_parts
     else:
-        raise FileNotFoundError("No fil uploaded")
+        raise FileNotFoundError("No file uploaded")
     
 ##initialize our streamlit app
 
